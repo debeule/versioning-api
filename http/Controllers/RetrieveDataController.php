8188a4531b7116b634c4e224cbb0364fb4ebd195
debeule <debeulematthias@gmail.com>
2024-02-02 13:24:39 +0100

    Fixed folder capitalization

---
+++ http/Controllers/RetrieveDataController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class RetrieveDataController extends Controller
+{
+    public function __invoke()
+    {
+        $client = new Client(
+            [
+                'base_uri' => 'http://api.example.com',
+                
+            ]
+        );
+        $response = $client->get('endpoint_X');
+        $data = $response->getBody()->getContents();
+        
+        Process the data
+        
+        return $data;
+    }
+}
