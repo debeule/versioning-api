6a05f7d160e1d5c45f76636a4049af868db91ac3
debeule <debeulematthias@gmail.com>
2024-02-07 08:54:32 +0100

    Added jobs to scheduler

--- console/Kernel.php
+++ console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new \App\Jobs\SyncSportsDomainJob())->Daily();
+        $schedule->job(new \App\Jobs\SyncSchoolsDomainJob())->Daily();
     }
 
     /**
