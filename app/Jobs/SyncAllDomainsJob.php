c24a8feeb306496a026ebbfbb2b4ccbd62bccb43
debeule <debeulematthias@gmail.com>
2024-02-08 11:25:16 +0100

    Restructured sync job

---
+++ app/Jobs/SyncAllDomainsJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use App\Schools\SyncSchoolsDomainCommand;
+use App\Sports\SyncSportsDomainCommand;
+
+class SyncAllDomainsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle(): void
+    {
+        $syncSchools = new SyncSchoolsDomainCommand();
+        $syncSchools();
+
+        $syncSports = new SyncSportsDomainCommand();
+        $syncSports();
+    }
+}
