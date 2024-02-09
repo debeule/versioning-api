9281d13ee431ab545446adec7efb6f2b02d15733
debeule <debeulematthias@gmail.com>
2024-02-09 12:27:52 +0100

    Add methods to run commands

--- app/Testing/TestCase.php
+++ app/Testing/TestCase.php
@@ -7,4 +7,22 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @param object $class
+     * @return mixed
+     */
+    public function handle(object $class): mixed
+    {
+        return $this->app->call([$class, 'handle']);
+    }
+
+    /**
+     * @param object $class
+     * @return mixed
+     */
+    public function dispatch(object $class): mixed
+    {
+        return $this->app->call([$class, '__invoke']);
+    }
 }
