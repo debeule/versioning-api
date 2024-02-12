51c990ba685018c4b2121d9aae2d28b9d4e0b4f1
debeule <debeulematthias@gmail.com>
2024-02-12 10:25:13 +0100

    docker redis error fix

--- deploy/components/app/setup.sh
+++ deploy/components/app/setup.sh
@@ -1,18 +1,20 @@
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 apk --no-cache add \
+    make \
+    g++ \
+    autoconf \
     mysql-client \
     sqlite \
     libgomp \
     pcre-dev \
-    autoconf \
-    imagemagick-dev \   
-    build-base
+    imagemagick-dev
 
 # Install PHP extensions
-docker-php-ext-install pdo_mysql pcntl  \
-    && pecl install redis \
-    && docker-php-ext-enable redis
+docker-php-ext-install pdo_mysql pcntl
+
+# Install redis
+pecl install redis
 
 addgroup -g 1000 -S app \
   && adduser -u 1000 -S app -G app \
