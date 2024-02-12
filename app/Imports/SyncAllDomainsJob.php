88ee8a03f71fa2f06cacbb4c0b8353b8fc4585ec
debeule <debeulematthias@gmail.com>
2024-02-12 13:49:05 +0100

    Moved Objects and Jobs to Imports namespace

--- app/Imports/SyncAllDomainsJob.php
+++ app/Imports/SyncAllDomainsJob.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Jobs;
+namespace App\Imports;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
--- app/Imports/Version.php
+++ app/Imports/Version.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Objects;
+namespace App\Imports;
 
 use DateTimeImmutable;
 
