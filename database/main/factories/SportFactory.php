964b95c1f8b7ef1274b8c66f005a125d04ca4111
debeule <debeulematthias@gmail.com>
2024-02-13 14:07:20 +0100

    namespace fix

--- database/main/factories/SportFactory.php
+++ database/main/factories/SportFactory.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Database\Main\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Sports\Sport;
+use App\Sport\Sport;
 
 final class SportFactory extends Factory
 {
