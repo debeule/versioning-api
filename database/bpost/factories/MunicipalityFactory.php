5a42a1661789745c3aceba42ab08a6a66b32b95a
debeule <debeulematthias@gmail.com>
2024-02-20 17:25:37 +0100

    Added factory for bpost municipality

---
+++ database/bpost/factories/MunicipalityFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Bpost\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Bpost\Municipality;
+
+final class MunicipalityFactory extends Factory
+{
+    protected $model = Municipality::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'Postcode' => 2000,
+            'Plaatsnaam' => 'Beerse',
+            'Deelgemeente' => 'Ja',
+            'HoofdGemeente' => 'Vlimmeren',
+            'Provincie' => $this->faker->randomElement(['Antwerpen', 'Limburg', 'Oost-Vlaanderen', 'Vlaams-Brabant', 'West-Vlaanderen']),
+        ];
+    }
+
+    public function withoutHeadMunicipality(): self
+    {
+        return $this->state(['
+            Deelgemeente' => 'Nee',
+            'HoofdGemeente' => null,
+        ]);
+    }
+}
