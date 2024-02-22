4f985745927ceb3792e84f7d9f825ae045c7d1d0
debeule <debeulematthias@gmail.com>
2024-02-22 11:04:19 +0100

    Added config bool for downloading municipalities excel file

--- config/app.php
+++ config/app.php
@@ -5,6 +5,8 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
+    'import_municipalities' => env('IMPORT_MUNICIPALITIES', true),
+
     /*
     |--------------------------------------------------------------------------
     | Application Name
