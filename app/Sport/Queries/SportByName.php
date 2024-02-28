24f8fc7782c921e88b48d967f69bba4eb9b5cfa0
debeule <debeulematthias@gmail.com>
2024-02-28 12:16:20 +0100

    Added first reworked query object

--- app/Sport/Queries/SportByName.php
+++ app/Sport/Queries/SportByName.php
@@ -4,25 +4,57 @@ declare(strict_types=1);
 
 namespace App\Sport\Queries;
 
+use App\Extensions\Eloquent\Scopes\FromVersion;
+use App\Extensions\Eloquent\Scopes\HasName;
 use App\Sport\Sport;
 use Illuminate\Database\Eloquent\Builder;
 use App\Imports\Values\Version;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 final class SportByName
 {
     public function __construct(
-        public Version $version = new Version()
+        public HasName $hasName = new HasName(''),
+        public FromVersion $fromVersion = new FromVersion,
     ) {}
 
-    public function query(string $name): Builder
+    public function query(): Builder
     {
-        $sportQuery = Sport::query()->where('name', '=', $name);
-
-        return $this->version->versionQuery($sportQuery);
+        return Sport::query()
+            ->tap($this->hasName)
+            ->when($this->fromVersion, $this->fromVersion);
     }
 
-    public function find(string $name): ?Sport
+    public function fromVersion(?string $versionString): self
     {
-        return $this->query($name)->first();
+        return new self(
+            $this->hasName,
+            new FromVersion(new Version($versionString)),
+        );
+    }
+
+    public function hasName(string $name): self
+    {
+        return new self(
+            new HasName($name),
+            $this->fromVersion,
+        );
+    }
+
+    public function get(): Sport
+    {
+        return $this->query()->firstOrFail();
+    }
+
+    public function find(): ?Sport
+    {
+        try 
+        {
+            return $this->get();
+        } 
+        catch (ModelNotFoundException) 
+        {
+            return null;
+        }
     }
 }
