40bee4a1df8bc5835e27f5fee01914627dcd6e97
debeule <debeulematthias@gmail.com>
2024-02-28 14:37:13 +0100

    Added scopes for regionNumber and postal

---
+++ app/Extensions/Eloquent/Scopes/HasPostalCode.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Extensions\Eloquent\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+
+final readonly class HasPostalCode
+{
+    public function __construct(
+        private string $value = '',
+    ) {}
+
+    public function __invoke(Builder $query): Builder
+    {
+        return $query->where('postal_code', '=', $this->value);
+    }
+}
---
+++ app/Extensions/Eloquent/Scopes/HasRegionNumber.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Extensions\Eloquent\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+
+final readonly class HasRegionNumber
+{
+    public function __construct(
+        private string $value = '',
+    ) {}
+
+    public function __invoke(Builder $query): Builder
+    {
+        return $query->where('region_number', '=', $this->value);
+    }
+}
