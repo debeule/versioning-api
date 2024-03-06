2167ea6c0f9f4444191e22ce9dcee17ce10c2f3b
debeule <debeulematthias@gmail.com>
2024-03-06 11:14:11 +0100

    Fixed wrong port for db-testing connection

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'db-testing'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '3307'),
             'database' => env('DB_DATABASE', 'sport-na-school'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'sport-na-school'),
