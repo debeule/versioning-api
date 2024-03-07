09916b7a01da43d7ca4f9fdbcb41dbe7bce82ceb
debeule <debeulematthias@gmail.com>
2024-03-07 12:44:37 +0100

    Updated AllMunicipalities query to use new services

--- app/Bpost/Queries/AllMunicipalities.php
+++ app/Bpost/Queries/AllMunicipalities.php
@@ -4,86 +4,30 @@ declare(strict_types=1);
 
 namespace App\Bpost\Queries;
 
-use App\Bpost\Municipality;
-use Excel;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Storage;
+use App\Bpost\Services\DownloadMunicipalities;
+use App\Bpost\Services\StoreMunicipalities;
+use App\Bpost\Services\ImportMunicipalities;
 
 final class AllMunicipalities
 {
-    private string $filePath = 'municipalities.xls';
-
-    /** @return array<string> */
-    public function query(): Array
-    {
-        if (config('tatooine.import_municipalities'))
-        {
-            $this->importMunicipalitiesFile();
-        }   
-        
-        $data = Excel::toArray(new Request(), $this->filePath, null, \Maatwebsite\Excel\Excel::XLS)[0];
-
-        return $data;
+    public function __construct(
+        private DownloadMunicipalities $download = new DownloadMunicipalities,
+        private StoreMunicipalities $store = new StoreMunicipalities,
+        private ImportMunicipalities $import = new ImportMunicipalities,
+    ) {
+        $this->filePath = (string) $bpostUri;
     }
 
-    public function get(): Object
+    public function query()
     {
-        $data = $this->query();
+        $file = $this->download->get();
+        $this->store->get($file);
         
-        $municipalities = collect();
-        
-        foreach ($data as $key => $value) 
-        {
-            $allowedProvinces = ['vlaams-brabant', 'west-vlaanderen', 'oost-vlaanderen', 'antwerpen', 'limburg'];
-
-            if ($key == 0 || $value[4] == null) 
-            {
-                continue;
-            }
-
-            if ( ! in_array(strtolower($value[4]), $allowedProvinces)) 
-            {
-                continue;
-            }
-            
-            try 
-            {
-                $municipality = new Municipality;
-                
-                $municipality->Plaatsnaam = $value[1];
-                $municipality->Postcode = $value[0];
-                $municipality->Provincie = strtolower($value[4]);
-
-                if ($value[2] === 'Ja') 
-                {
-                    $municipality->Hoofdgemeente = $value[3];
-                }
-                
-                $municipalities->push($municipality);
-            } 
-
-            catch (\Throwable $th) 
-            {
-                continue;
-            }
-        }
-
-        return $municipalities;
+        return $this->import->get();
     }
 
-    public function importMunicipalitiesFile(): bool
+    public function get()
     {
-        $url = 'https://www.bpost2.be/zipcodes/files/zipcodes_alpha_nl_new.xls';
-
-        $file = Http::withOptions(['verify' => false])->get($url)->body();
-
-        if (File::exists($this->filePath)) 
-        {
-            File::delete($this->filePath);
-        }
-        
-        return Storage::disk('local')->put($this->filePath, $file);
+        return $this->query();
     }
 }
