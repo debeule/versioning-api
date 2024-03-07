c6797d14f9a76bc8b2d85123809f60b7ee0a53a8
debeule <debeulematthias@gmail.com>
2024-03-07 14:40:26 +0100

    Modified ProvinceGroup for static use

--- app/Imports/Values/ProvinceGroup.php
+++ app/Imports/Values/ProvinceGroup.php
@@ -7,22 +7,22 @@ namespace App\Imports\Values;
 
 final class ProvinceGroup
 {
-    private array $flemishProvinces = ['antwerpen', 'limburg', 'oost-vlaanderen', 'vlaams-brabant', 'west-vlaanderen'];
-    private array $walloonProvinces = ['waals-brabant', 'henegouwen', 'luik', 'luxemburg', 'namen'];
-    private array $brussels = ['brussel'];
+    private static array $flemishProvinces = ['antwerpen', 'limburg', 'oost-vlaanderen', 'vlaams-brabant', 'west-vlaanderen'];
+    private static array $walloonProvinces = ['waals-brabant', 'henegouwen', 'luik', 'luxemburg', 'namen'];
+    private static array $brussels = ['brussel'];
 
     public function __construct(
         private array $value = [],
     ) {}
 
-    public function allProvinces(): self
+    public static function allProvinces(): self
     {
-        return new self(array_merge($this->flemishProvinces, $this->walloonProvinces, $this->brussels));
+        return new self(array_merge(self::$flemishProvinces, self::$walloonProvinces, self::$brussels));
     }
 
-    public function flemishProvinces(): self
+    public static function flemishProvinces(): self
     {
-        return new self($this->flemishProvinces);
+        return new self(self::$flemishProvinces);
     }
 
     public function get(): array
