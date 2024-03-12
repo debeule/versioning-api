b89973a29b963528c842c97a8e4eb4b11c90bc43
debeule <debeulematthias@gmail.com>
2024-03-12 13:56:53 +0100

    Added Test for arraytocollection pipe

---
+++ app/Services/Pipes/ArrayToCollectionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Pipes;
+
+use App\Testing\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use Database\Bpost\Factories\MunicipalityFactory as BpostMunicipalityFactory;
+use Illuminate\Support\Collection;
+use App\Services\Pipes\ArrayToCollection;
+use App\Bpost\Municipality;
+use Illuminate\Pipeline\Pipeline;
+
+final class ArrayToCollectionTest extends TestCase
+{
+    #[Test]
+    public function returnsCollectionOfobjectType(): void
+    {
+        $municiaplity = BpostMunicipalityFactory::new()->makeArray();
+
+        $data = [
+            'spreadsheetArray' => [$municiaplity],
+            'objectType' => Municipality::class,
+        ];
+
+        $result = app(Pipeline::class)
+            ->send($data)
+            ->through([ArrayToCollection::class])
+            ->thenReturn();
+            
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertInstanceOf(Municipality::class, $result->first());
+    }
+
+}
