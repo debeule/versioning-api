dd3ac5a5790f8eb9a55edcee257c56dcff07de50
debeule <debeulematthias@gmail.com>
2024-03-12 15:39:03 +0100

    Added tests for pipes

---
+++ app/Services/Pipes/RetrieveFromSourceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Pipes;
+
+use App\Testing\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use Database\Bpost\Factories\MunicipalityFactory as BpostMunicipalityFactory;
+use Illuminate\Support\Collection;
+use App\Services\Pipes\RetrieveFromSource;
+use App\Bpost\Municipality;
+use Illuminate\Pipeline\Pipeline;
+
+final class RetrieveFromSourceTest extends TestCase
+{
+    #[Test]
+    public function returnsFileContentsFromSource(): void
+    {
+        $data = [
+            'source' => config('tatooine.testSource'),
+        ];
+
+        $result = app(Pipeline::class)
+            ->send($data)
+            ->through([RetrieveFromSource::class])
+            ->thenReturn();
+
+        // dd($result);
+
+        #TODO: find a way to download test file and test response
+        $this->assertTrue(true);
+    }
+
+}
---
+++ app/Services/Pipes/SanitizeSpreadsheetArrayTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Pipes;
+
+use App\Testing\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use Database\Bpost\Factories\MunicipalityFactory as BpostMunicipalityFactory;
+use Illuminate\Support\Collection;
+use App\Services\Pipes\ArrayToCollection;
+use App\Bpost\Municipality;
+use Illuminate\Pipeline\Pipeline;
+
+final class SanitizeSpreadsheetArrayTest extends TestCase
+{
+    #[Test]
+    public function returnsCollectionOfobjectType(): void
+    {
+        $municiaplities = BpostMunicipalityFactory::new()->count(2, true);
+        $municiaplities[0]['4'] = 'non existant province';
+
+        $data = [
+            'spreadsheetArray' => $municiaplities,
+        ];
+
+        $result = app(Pipeline::class)
+            ->send($data)
+            ->through([SanitizeSpreadsheetArray::class])
+            ->thenReturn();
+            
+        $this->assertisArray($result);
+        $this->assertEquals(count($result), 1);
+    }
+
+}
