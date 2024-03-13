ddfbf729f4e37299fbf58badfc4c4887f76533ed
debeule <debeulematthias@gmail.com>
2024-03-13 16:50:05 +0100

    Added testing xls file url to config

--- config/tatooine.php
+++ config/tatooine.php
@@ -2,4 +2,5 @@
 
 return [
     'should_import' => env('IMPORT_MUNICIPALITIES', TRUE),
+    'test_file_url' => 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_100KB_XLS.xls',
 ];
