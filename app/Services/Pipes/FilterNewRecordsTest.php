bed11eb1a352e14b9089beca0e497144f4a00a44
debeule <debeulematthias@gmail.com>
2024-03-20 12:22:27 +0100

    Added tests for FIlterNewRecords pipe

--- app/Services/Pipes/FilterNewRecordsTest.php
+++ app/Services/Pipes/FilterNewRecordsTest.php
@@ -6,13 +6,54 @@ namespace App\Services\Pipes;
 
 use App\Testing\TestCase;
 use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Pipeline\Pipeline;
+use App\Kohera\School as KoheraSchool;
+use App\School\School;
+use App\School\Commands\CreateSchool;
+use Illuminate\Support\Collection;
+use Database\Kohera\Factories\SchoolFactory as KoheraSchoolFactory;
+use Database\Main\Factories\AddressFactory;
 
 final class FilterNewRecordsTest extends TestCase
 {
     #[Test]
-    public function itReturnsString(): void
+    public function createContainsNewRecords(): void
     {
-        $this->assertTrue(false);
+        $data = [
+            'records' => KoheraSchoolFactory::new()->count(3)->create(),
+            'existingRecords' => School::get(),
+        ];
+
+        $result = app(Pipeline::class)
+            ->send($data)
+            ->through([FilterNewRecords::class])
+            ->thenReturn();
+            
+        $this->assertInstanceOf(Collection::class, $result['create']);
+        $this->assertInstanceOf(KoheraSchool::class, $result['create']->first());
+        $this->assertEquals(3, $result['create']->count());
     }
 
+    #[Test]
+    public function createDoesNotContainExistingRecords(): void
+    {
+        $koheraSchools = KoheraSchoolFactory::new()->count(3)->create();
+
+        $koheraSchoolRecordId = 'school-' . (string) $koheraSchools->first()->recordId();
+        AddressFactory::new()->withId($koheraSchoolRecordId)->create();
+
+        $this->DispatchSync(new CreateSchool($koheraSchools->first()));
+
+        $data = [
+            'records' => $koheraSchools,
+            'existingRecords' => School::get(),
+        ];  
+
+        $result = app(Pipeline::class)
+            ->send($data)
+            ->through([FilterNewRecords::class])
+            ->thenReturn();
+        
+        $this->assertEquals(2, $result['create']->count());
+    }
 }
