d626fb06e7a1fcdcf37d8ea2435f22dd911f7b1e
debeule <debeulematthias@gmail.com>
2024-03-20 13:27:26 +0100

    Added test to fromVersion scope

--- app/Extensions/Eloquent/Scopes/FromVersionTest.php
+++ app/Extensions/Eloquent/Scopes/FromVersionTest.php
@@ -6,12 +6,29 @@ namespace App\Extensions\Eloquent\Scopes;
 
 use App\Testing\TestCase;
 use PHPUnit\Framework\Attributes\Test;
+use Database\Main\Factories\SchoolFactory;
+use App\School\School;
 
 final class FromVersionTest extends TestCase
 {
     #[Test]
     public function fromVersionQueryReturnsCorrectRecordVersion(): void
     {
-        $this->assertTrue(false);
+        $school = SchoolFactory::new()->create();
+
+        $school2 = $school->replicate();
+        $school2->save();
+
+        $school->delete();
+
+        $school2->created_at = now()->addDays(2)->toDateTimeString();
+
+        $fromVersion = new FromVersion();
+        
+        $result = School::query()->when($fromVersion, $fromVersion)->get();
+
+        $this->assertSoftDeleted($school);
+        $this->assertCount(1, $result);
+        $this->assertEquals($school2->record_id, $result->first()->record_id);
     }
 }
