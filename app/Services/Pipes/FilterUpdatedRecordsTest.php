d79f5b43c404731b122de6cb69f9a358cdb908f1
debeule <debeulematthias@gmail.com>
2024-03-20 12:55:19 +0100

    Added missing tests to services

--- app/Services/Pipes/FilterUpdatedRecordsTest.php
+++ app/Services/Pipes/FilterUpdatedRecordsTest.php
@@ -6,13 +6,65 @@ namespace App\Services\Pipes;
 
 use App\Testing\TestCase;
 use PHPUnit\Framework\Attributes\Test;
+use App\School\School;
+use App\Kohera\School as KoheraSchool;
+use Database\Kohera\Factories\SchoolFactory as KoheraSchoolFactory;
+use Database\Main\Factories\AddressFactory;
+use Illuminate\Pipeline\Pipeline;
+use Illuminate\Support\Collection;
+use App\School\Commands\CreateSchool;
 
 final class FilterUpdatedRecordsTest extends TestCase
 {
     #[Test]
-    public function itReturnsString(): void
+    public function updateContainsUpdatedRecords(): void
     {
-        $this->assertTrue(false);
+        $koheraSchools = KoheraSchoolFactory::new()->count(3)->create();
+
+
+        foreach($koheraSchools as $koheraSchool) 
+        {
+            $koheraSchoolRecordId = 'school-' . (string) $koheraSchool->recordId();
+            AddressFactory::new()->withId($koheraSchoolRecordId)->create();
+
+            $this->DispatchSync(new CreateSchool($koheraSchool));
+
+            $koheraSchool->update(['Name' => 'Updated Name']);
+        }
+
+        $result = app(Pipeline::class)
+            ->send([
+                'records' => $koheraSchools,
+                'existingRecords' => School::get(),
+            ])
+            ->through([FilterUpdatedRecords::class])
+            ->thenReturn();
+            
+        $this->assertInstanceOf(Collection::class, $result['update']);
+        $this->assertInstanceOf(KoheraSchool::class, $result['update']->first());
+        $this->assertEquals(3, $result['update']->count());
     }
 
+    #[Test]
+    public function updateDoesNotContainNewOrDeletedRecords(): void
+    {
+        $koheraSchools = KoheraSchoolFactory::new()->count(3)->create();
+
+        $koheraSchoolRecordId = 'school-' . (string) $koheraSchools->first()->recordId();
+        AddressFactory::new()->withId($koheraSchoolRecordId)->create();
+
+        $this->DispatchSync(new CreateSchool($koheraSchools->first()));
+
+        $data = [
+            'records' => $koheraSchools,
+            'existingRecords' => School::get(),
+        ];  
+
+        $result = app(Pipeline::class)
+            ->send($data)
+            ->through([FilterNewRecords::class])
+            ->thenReturn();
+        
+        $this->assertEquals(2, $result['create']->count());
+    }
 }
--- app/Services/SpreadsheetToCollectionTest.php
+++ app/Services/SpreadsheetToCollectionTest.php
@@ -6,13 +6,18 @@ namespace App\Services;
 
 use App\Testing\TestCase;
 use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Support\Collection;
+use App\Location\Municipality;
 
 final class SpreadsheetToCollectionTest extends TestCase
 {
-    #[Test]
-    public function itReturnsString(): void
-    {
-        $this->assertTrue(false);
-    }
+    private string $storagePath = 'excel/TestFile.xls';
 
+    #[Test]
+    public function itReturnsCollection(): void
+    {
+        $result = SpreadsheetToCollection::setup($this->storagePath, Municipality::class)->pipe();
+
+        $this->assertInstanceOf(Collection::class, $result);
+    }
 }
