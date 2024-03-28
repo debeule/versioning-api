2aa13f667a8003366ec0c7301a46e843baf9db34
debeule <debeulematthias@gmail.com>
2024-03-28 14:58:24 +0100

    Reworked Sync class for municipalities

---
+++ app/Location/Commands/SyncMunicipalities.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Location\Commands;
+
+use App\Location\Queries\MunicipalityDiff;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\Imports\Queries\ExternalMunicipalities;
+
+final class SyncMunicipalities
+{
+    use DispatchesJobs;
+
+    public function __invoke(MunicipalityDiff $municipalityDiff): void
+    {
+        foreach ($municipalityDiff->additions() as $koheraMunicipality) 
+        {
+            $this->dispatchSync(new CreateMunicipality($koheraMunicipality));
+        }
+
+        foreach ($municipalityDiff->deletions() as $municipality) 
+        {
+            $this->dispatchSync(new SoftDeleteMunicipality($municipality));
+        }
+
+        foreach ($municipalityDiff->updates() as $koheraMunicipality) 
+        {
+            $this->dispatchSync(new SoftDeleteMunicipality(Municipality::where('record_id', $koheraMunicipality->recordId())->first()));
+            $this->dispatchSync(new CreateMunicipality($koheraMunicipality));
+        }
+    }
+}
