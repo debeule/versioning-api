a6e737c10a87878213b3c463a2cb0ff003c0825e
debeule <debeulematthias@gmail.com>
2024-04-04 13:57:18 +0200

    Added missing query object tests

--- app/School/Queries/AllAddressesTest.php
+++ app/School/Queries/AllAddressesTest.php
@@ -2,16 +2,38 @@
 
 declare(strict_types=1);
 
-namespace App\Location\Queries;
+namespace App\School\Queries;
 
+use App\School\Address;
 use App\Testing\TestCase;
+use Database\Main\Factories\AddressFactory;
+use Illuminate\Support\Collection;
 use PHPUnit\Framework\Attributes\Test;
 
 final class AllAddressesTest extends TestCase
 {
     #[Test]
-    public function addtestsHere(): void
+    public function ItCanGetAllAddresses(): void
     {
-        $this->assertEquals(true, false);
+        AddressFactory::new()->count(3)->create();
+
+        $allAddresses = new AllAddresses;
+        $result = $allAddresses->get();
+
+        $allAddressRecords = Address::get();
+
+        $this->assertEquals($allAddressRecords->count(), $result->count());
+    }
+
+    #[Test]
+    public function ItReturnsCollectionOfAddresses(): void
+    {
+        AddressFactory::new()->count(3)->create();
+
+        $allAddresses = new AllAddresses;
+        $result = $allAddresses->get();
+
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertInstanceOf(Address::class, $result->first());
     }
 }
--- app/School/Queries/AllBillingProfilesTest.php
+++ app/School/Queries/AllBillingProfilesTest.php
@@ -2,16 +2,38 @@
 
 declare(strict_types=1);
 
-namespace App\Location\Queries;
+namespace App\School\Queries;
 
+use App\School\billingProfile;
 use App\Testing\TestCase;
+use Database\Main\Factories\billingProfileFactory;
+use Illuminate\Support\Collection;
 use PHPUnit\Framework\Attributes\Test;
 
-final class AllBillingProfilesTest extends TestCase
+final class AllbillingProfilesTest extends TestCase
 {
     #[Test]
-    public function addtestsHere(): void
+    public function ItCanGetAllbillingProfiles(): void
     {
-        $this->assertEquals(true, false);
+        billingProfileFactory::new()->count(3)->create();
+
+        $allbillingProfiles = new AllbillingProfiles;
+        $result = $allbillingProfiles->get();
+
+        $allbillingProfileRecords = billingProfile::get();
+
+        $this->assertEquals($allbillingProfileRecords->count(), $result->count());
+    }
+
+    #[Test]
+    public function ItReturnsCollectionOfbillingProfiles(): void
+    {
+        billingProfileFactory::new()->count(3)->create();
+
+        $allbillingProfiles = new AllbillingProfiles;
+        $result = $allbillingProfiles->get();
+
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertInstanceOf(billingProfile::class, $result->first());
     }
 }
