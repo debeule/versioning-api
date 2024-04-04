c05d3471b3a0f7e581d033871ab53302f726b35f
debeule <debeulematthias@gmail.com>
2024-04-04 11:14:43 +0200

    Added tests to SportDiff

---
+++ app/Sport/Queries/SportDiffTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Sport\Queries;
+
+use App\Testing\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use App\Imports\Queries\ExternalSports;
+use App\Kohera\Queries\AllSports;
+use App\Sport\Commands\CreateSport;
+use Database\Kohera\Factories\SportFactory as KoheraSportFactory;
+use App\Kohera\Sport as KoheraSport;
+use App\Sport\Sport;
+
+final class SportDiffTest extends TestCase
+{
+    #[Test]
+    public function itReturnsCorrectAdditions(): void
+    {
+        $koheraSports = KoheraSportFactory::new()->count(3)->create();
+
+        $this->dispatchSync(new CreateSport($koheraSports->first()));
+
+        $sportDiff = app(SportDiff::class);
+
+        $result = $sportDiff->additions();
+
+        $this->assertInstanceOf(KoheraSport::class, $result->first());
+        $this->assertEquals(2, $result->count());
+    }
+    
+    #[Test]
+    public function itReturnsCorrectDeletions(): void
+    {
+        $koheraSports = KoheraSportFactory::new()->count(3)->create();
+
+        $removedKoheraSport = $koheraSports->shift();
+
+        $this->dispatchSync(new CreateSport($removedKoheraSport));
+
+        $sportDiff = app(SportDiff::class);
+
+        $result = $sportDiff->deletions();
+
+        $this->assertInstanceOf(Sport::class, $result->first());
+        $this->assertEquals(1, $result->count());
+        $this->assertTrue($result->where('head_sport', $removedKoheraSport->headSport())->isNotEmpty());
+    }
+    
+    #[Test]
+    public function itReturnsCorrectUpdates(): void
+    {
+        $koheraSports = KoheraSportFactory::new()->count(3)->create();
+
+        $this->dispatchSync(new CreateSport($koheraSports->first()));
+
+        $newName = "new head sport";
+        $koheraSports->first()->Sportkeuze = $newName;
+        $koheraSports->first()->save();
+
+        $sportDiff = app(SportDiff::class);
+
+        $result = $sportDiff->updates();
+
+        $this->assertInstanceOf(KoheraSport::class, $result->first());
+        $this->assertEquals(1, $result->count());
+        $this->assertEquals($newName, $result->first()->Sportkeuze);
+    }
+}
