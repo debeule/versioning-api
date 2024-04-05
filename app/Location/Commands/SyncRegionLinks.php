33405a194651318a2501468c0d41ab6f9d053fd6
debeule <debeulematthias@gmail.com>
2024-04-05 13:38:17 +0200

    Renamed region to externalRegion

--- app/Location/Commands/SyncRegionLinks.php
+++ app/Location/Commands/SyncRegionLinks.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Location\Commands;
 
 use App\Location\Queries\RegionLinksDiff;
-use App\Location\Region;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
 final class SyncRegionLinks
@@ -14,9 +13,9 @@ final class SyncRegionLinks
 
     public function __invoke(RegionLinksDiff $regionLinksDiff): void
     {
-        foreach ($regionLinksDiff->toLink() as $region) 
+        foreach ($regionLinksDiff->toLink() as $externalRegion) 
         {
-            $this->dispatchSync(new LinkRegion($region));
+            $this->dispatchSync(new LinkRegion($externalRegion));
         }
     }
 }
